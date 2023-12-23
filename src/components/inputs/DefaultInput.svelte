9ee2e55f588b686a2230daeeb7a0f39eeb229c57
Guilherme Saliba <salibagui19@gmail.com>
2023-12-23 20:21:11 -0300

    :tada: first commit

---
+++ src/components/inputs/DefaultInput.svelte
@@ -0,0 +1,13 @@
+<script>
+  /**
+   * @type {string}
+   */
+  export let label;
+</script>
+
+<div class="container">
+  <label for="default--input">{label}</label>
+  <input id="default--input" type="text" {...$$props} />
+</div>
+
+<style></style>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,52 @@
+<script>
+  import DefaultInput from '../../components/inputs/DefaultInput.svelte';
+</script>
+
+<div class="main">
+  <div class="login--wrapper">
+    <h1>Login</h1>
+    <DefaultInput label="username" type="text" />
+    <DefaultInput label="password" type="password" />
+    <button>Login</button>
+  </div>
+</div>
+
+<style>
+  .main {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+    background-color: #f0f0f0;
+  }
+
+  .login--wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+    background: #fff;
+    border-radius: 12px;
+    padding: 32px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    max-width: 400px;
+  }
+
+  h1 {
+    margin-bottom: 16px;
+    color: #333;
+  }
+
+  button {
+    background-color: #4caf50;
+    color: #fff;
+    padding: 10px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+  }
+
+  button:hover {
+    background-color: #45a049;
+  }
+</style>
